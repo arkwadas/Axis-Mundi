a042fdb8d2a3cb3d0f970160ef9a517f19c3b690
arkwadas <ark.wadas@gmail.com>
2023-04-01 02:17:31 +0200

    up dialogue

---
+++ Assets/01Script/UI/DialogueUI.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+using UnityEngine.UI;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI AIText;
+        [SerializeField] Button nextButton;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            AIText.text = playerConversant.GetText();
+            nextButton.onClick.AddListener(Next);
+
+            UpdateUI();
+        }
+
+        // Update is called once per frame
+        void Next()
+        {
+            playerConversant.Next();
+            UpdateUI();
+        }
+
+        void UpdateUI()
+        {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
+        }
+    }
+}
